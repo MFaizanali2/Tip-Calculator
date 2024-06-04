398ba10bf25baa71ba205c714ac7b547d108518a
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-04 11:31:28 +0500

    complete
    continue

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+function Calculate(){
+    let billamount = document.getElementById("bill").value;
+    let tipamount = document.getElementById("tip").value;
+    let total = document.getElementById("total");
+
+    let sum = billamount * (1 + tipamount / 100);
+    total.innerText = `  $${sum}`
+
+}
